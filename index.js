e935e9176c66f8f3472e513ffd131d6ab19e00de
Ej Myles <ejelymyles@gmail.com>
2023-05-17 16:47:30 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+    let name = "EJ"
+    let email = "ejelymyles@gmail.com"
+
+function submitData(name, email ){
+  
+    return fetch(`http://localhost:3000/users`, {
+        method: `POST`,
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+    },
+        body: JSON.stringify({
+            name,
+            email
+        }),
+    })
+        .then (response => response.json())
+        .then (data => {
+            const newId = data.id;
+            const idElement = document.createElement("p");
+            idElement.textContent = `New ID: ${newId}`;
+            document.body.appendChild(idElement); 
+    }) 
+        .catch(error => {
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = error.message;
+            document.body.appendChild(errorMessage);
+        });
+};
+
